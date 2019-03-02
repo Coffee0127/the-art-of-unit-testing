67b298d7e8c3ea7f1eb521e4af47b741d1649783
Bo-Xuan Fan <coffee0127@gmail.com>
2019-03-02 16:28:09 +0800

    增加正向的測試

--- src/test/java/com/osherove/ch2/LogAnalyzerTest.java
+++ src/test/java/com/osherove/ch2/LogAnalyzerTest.java
@@ -18,4 +18,22 @@ public class LogAnalyzerTest {
         Assert.assertFalse(result);
     }
 
+    @Test
+    public void IsValidLogFileName_GoodExtensionUppercase_ReturnsTrue() {
+        LogAnalyzer analyzer = new LogAnalyzer();
+
+        boolean result = analyzer.isValidLogFileName("filewithgoodextension.SLF");
+
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void IsValidLogFileName_GoodExtensionLowercase_ReturnsTrue() {
+        LogAnalyzer analyzer = new LogAnalyzer();
+
+        boolean result = analyzer.isValidLogFileName("filewithgoodextension.slf");
+
+        Assert.assertTrue(result);
+    }
+
 }
