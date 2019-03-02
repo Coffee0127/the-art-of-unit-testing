09ee4bce426aef173dc641cbf47d8b923f0174e7
Bo-Xuan Fan <coffee0127@gmail.com>
2019-03-02 16:41:12 +0800

    Move original test to NonParameterizedTest

--- src/test/java/com/osherove/ch2/LogAnalyzerTest.java
+++ src/test/java/com/osherove/ch2/LogAnalyzerTest.java
@@ -2,38 +2,43 @@ package com.osherove.ch2;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
 
+@RunWith(Enclosed.class)
 public class LogAnalyzerTest {
 
-    @Test
-    public void IsValidLogFileName_BadExtension_ReturnsFalse() {
-        // 單元測試包含了三個行為 (3A)
-        // 準備 (Arrange) 物件
-        LogAnalyzer analyzer = new LogAnalyzer();
+    public static class NonParameterizedTest {
+        @Test
+        public void IsValidLogFileName_BadExtension_ReturnsFalse() {
+            // 單元測試包含了三個行為 (3A)
+            // 準備 (Arrange) 物件
+            LogAnalyzer analyzer = new LogAnalyzer();
 
-        // 操作 (Act) 物件
-        boolean result = analyzer.isValidLogFileName("filewithbadextension.foo");
+            // 操作 (Act) 物件
+            boolean result = analyzer.isValidLogFileName("filewithbadextension.foo");
 
-        // 驗證 (Assert) 某件事符合預期
-        Assert.assertFalse(result);
-    }
+            // 驗證 (Assert) 某件事符合預期
+            Assert.assertFalse(result);
+        }
 
-    @Test
-    public void IsValidLogFileName_GoodExtensionUppercase_ReturnsTrue() {
-        LogAnalyzer analyzer = new LogAnalyzer();
+        @Test
+        public void IsValidLogFileName_GoodExtensionUppercase_ReturnsTrue() {
+            LogAnalyzer analyzer = new LogAnalyzer();
 
-        boolean result = analyzer.isValidLogFileName("filewithgoodextension.SLF");
+            boolean result = analyzer.isValidLogFileName("filewithgoodextension.SLF");
 
-        Assert.assertTrue(result);
-    }
+            Assert.assertTrue(result);
+        }
 
-    @Test
-    public void IsValidLogFileName_GoodExtensionLowercase_ReturnsTrue() {
-        LogAnalyzer analyzer = new LogAnalyzer();
+        @Test
+        public void IsValidLogFileName_GoodExtensionLowercase_ReturnsTrue() {
+            LogAnalyzer analyzer = new LogAnalyzer();
 
-        boolean result = analyzer.isValidLogFileName("filewithgoodextension.slf");
+            boolean result = analyzer.isValidLogFileName("filewithgoodextension.slf");
 
-        Assert.assertTrue(result);
+            Assert.assertTrue(result);
+        }
     }
 
 }
