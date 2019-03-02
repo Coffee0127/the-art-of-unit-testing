170c08d749e70e65394705ae846a67bc764bac11
Bo-Xuan Fan <coffee0127@gmail.com>
2019-03-02 20:39:21 +0800

    Extract makeCalc method

--- src/test/java/com/osherove/ch2/MemCalculatorTest.java
+++ src/test/java/com/osherove/ch2/MemCalculatorTest.java
@@ -7,12 +7,16 @@ public class MemCalculatorTest {
 
     @Test
     public void Sum_ByDefault_ReturnsZero() {
-        MemCalculator calc = new MemCalculator();
+        MemCalculator calc = makeCalc();
 
         int lastSum = calc.sum();
 
         // 驗證預設回傳值
         Assert.assertEquals(0, lastSum);
+    }
+
+    private MemCalculator makeCalc() {
+        return new MemCalculator();
     }
 
 }
