6345e6bb6c308275e8a324585c10c33f3b8d121e
Bo-Xuan Fan <coffee0127@gmail.com>
2019-03-02 17:05:14 +0800

    Add empty file name check

--- src/main/java/com/osherove/ch2/LogAnalyzer.java
+++ src/main/java/com/osherove/ch2/LogAnalyzer.java
@@ -3,6 +3,10 @@ package com.osherove.ch2;
 public class LogAnalyzer {
 
     public boolean isValidLogFileName(String fileName) {
+        if (fileName == null || fileName.length() == 0) {
+            throw new IllegalArgumentException("filename has to be provided");
+        }
+
         if (!fileName.toUpperCase().endsWith(".SLF")) {
             return false;
         }
