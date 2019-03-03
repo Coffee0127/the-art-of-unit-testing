9a11c7d0211bbf186269ab37b07455a3bb0d5778
Bo-Xuan Fan <coffee0127@gmail.com>
2019-03-03 16:03:02 +0800

    Read external configuration to check extensions

--- src/main/java/com/osherove/ch3/FileExtensionManager.java
+++ src/main/java/com/osherove/ch3/FileExtensionManager.java
@@ -1,13 +1,44 @@
 package com.osherove.ch3;
 
+import static java.util.stream.Collectors.toList;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import com.amihaiemil.eoyaml.Yaml;
+import com.amihaiemil.eoyaml.YamlSequence;
+
 public class FileExtensionManager {
+
+    private static final List<String> AVAILABLE_EXTENSIONS;
+
+    static {
+        try {
+            InputStream resourceAsStream = Thread.currentThread().getContextClassLoader()
+                .getResourceAsStream("com/osherove/ch3/fileNameExtensions.yml");
+            YamlSequence extensions = Yaml.createYamlInput(resourceAsStream)
+                .readYamlMapping()
+                .yamlSequence("extensions");
+            AVAILABLE_EXTENSIONS = extensions.children().stream()
+                .map(yamlNode -> yamlNode.toString().toUpperCase())
+                .collect(toList());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public boolean isValid(String fileName) {
         if (fileName == null || fileName.length() == 0) {
             throw new IllegalArgumentException("filename has to be provided");
         }
 
-        return fileName.toUpperCase().endsWith(".SLF");
+        for (String availableExtension : AVAILABLE_EXTENSIONS) {
+            if (fileName.toUpperCase().endsWith("." + availableExtension)) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
