86eed9dc0f98eea24d8199ae0a347a1ceeedddf3
Bo-Xuan Fan <coffee0127@gmail.com>
2019-03-03 20:14:21 +0800

    模擬 web 服務拋出例外

--- src/test/java/com/osherove/ch4/FakeWebService.java
+++ src/test/java/com/osherove/ch4/FakeWebService.java
@@ -3,10 +3,15 @@ package com.osherove.ch4;
 public class FakeWebService implements IWebService {
 
     String lastError;
+    RuntimeException toThrow;
 
     @Override
     public void logError(String message) {
         lastError = message;
+
+        if (toThrow != null) {
+            throw toThrow;
+        }
     }
 
 }
