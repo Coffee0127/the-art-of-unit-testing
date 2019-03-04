e898c747f987cd4a25dd62db3e2a840640c43c72
Bo-Xuan Fan <coffee0127@gmail.com>
2019-03-04 21:22:10 +0800

    使用 Mockito 產生假物件

--- src/test/java/com/osherove/ch5/LogAnalyzerTest.java
+++ src/test/java/com/osherove/ch5/LogAnalyzerTest.java
@@ -3,6 +3,8 @@ package com.osherove.ch5;
 import org.hamcrest.core.StringContains;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Matchers;
+import org.mockito.Mockito;
 
 public class LogAnalyzerTest {
 
@@ -17,6 +19,22 @@ public class LogAnalyzerTest {
 
         // 把假物件當模擬物件來使用並驗證
         Assert.assertThat(logger.lastError, StringContains.containsString("too short"));
+    }
+
+    @Test
+    public void Analyze_TooShortFileName_CallLogger_ByMockito() {
+        // 建立模擬物件
+        FakeLogger logger = Mockito.mock(FakeLogger.class);
+
+        LogAnalyzer analyzer = new LogAnalyzer(logger);
+        analyzer.setMinNameLength(6);
+        analyzer.analyze("a.txt");
+
+        // 使用 Mockito API 來驗證期望結果
+        // times(1) 代表某物件某方法應該被呼叫過一次
+        Mockito.verify(logger, Mockito.times(1))
+            // 預期 logError 傳入參數
+            .logError(Matchers.eq("Filename too short: a.txt"));
     }
 
     class FakeLogger implements ILogger {
