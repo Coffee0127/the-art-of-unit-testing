d812b5a3084e308e89d83b99a030efc99303acc6
Bo-Xuan Fan <coffee0127@gmail.com>
2019-03-04 21:34:53 +0800

    模擬拋出例外

--- src/test/java/com/osherove/ch5/MockitoBasics.java
+++ src/test/java/com/osherove/ch5/MockitoBasics.java
@@ -1,10 +1,15 @@
 package com.osherove.ch5;
 
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.mockito.Mockito;
 
 public class MockitoBasics {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     @Test
     public void Returns_ByDefault_WorksForHardCodedArgument() {
@@ -26,6 +31,21 @@ public class MockitoBasics {
             .thenReturn(true);
 
         Assert.assertTrue(fakeRules.isValidLogFileName("anything.txt"));
+    }
+
+    @Test
+    public void Returns_ArgAny_Throws() {
+        IFileNameRules fakeRules = Mockito.mock(IFileNameRules.class);
+
+        Mockito.when(fakeRules.isValidLogFileName(Mockito.anyString()))
+            // 模擬拋出例外
+            .thenThrow(new RuntimeException("fake exception"));
+
+        // 驗證拋出例外
+        thrown.expect(RuntimeException.class);
+        // thrown.expectMessage("fake exception");
+
+        fakeRules.isValidLogFileName("anything");
     }
 
 }
