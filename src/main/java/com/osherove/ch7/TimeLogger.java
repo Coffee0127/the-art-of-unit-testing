a603ce9182fbe31544ab8692526cc7a6d182db48
Bo-Xuan Fan <coffee0127@gmail.com>
2019-03-07 20:54:19 +0800

    使用 SystemTime 類別

--- src/main/java/com/osherove/ch7/TimeLogger.java
+++ src/main/java/com/osherove/ch7/TimeLogger.java
@@ -1,12 +1,12 @@
 package com.osherove.ch7;
 
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class TimeLogger {
 
     public static String createMessage(String info) {
-        return DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(LocalDateTime.now()) + " " + info;
+        // 產品程式碼使用 SystemTime
+        return DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(SystemTime.now()) + " " + info;
     }
 
 }
